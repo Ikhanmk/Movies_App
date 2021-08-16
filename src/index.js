f1779d9f70acc645d9d34ac9d157d91579160def
ikhanmk <ikhanmk@gmail.com>
2021-08-17 00:04:23 +0530

    Checkpoint-5

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
+import Details from './screens/details/Details';
 import Home from "./screens/home/Home";
 
+
 ReactDOM.render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Route exact path="/" component={Home}></Route>
+    <Route exact path="/details/:id" component={Details}></Route>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
